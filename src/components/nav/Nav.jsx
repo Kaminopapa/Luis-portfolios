95dca07b2ad531bbf295d319a8d2b984bab44639
Luis Hung <luishung1996@gmail.com>
2022-06-15 02:00:36 -0700

    updated new nav bar feature and intro animation time. when scroll down to the page, nav bar will track where is the user visiting

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,23 +1,95 @@
-import React from 'react'
-import './nav.css'
-import {HiOutlineHome} from "react-icons/hi"
-import {AiOutlineUser,AiOutlineMessage} from "react-icons/ai"
-import {BiBook} from "react-icons/bi"
-import {MdOutlinePhoto} from "react-icons/md"
-import { useState } from "react"
+import React, { useEffect, useState } from "react";
+import "./nav.css";
+import { HiOutlineHome } from "react-icons/hi";
+import { AiOutlineUser, AiOutlineMessage } from "react-icons/ai";
+import { BiBook } from "react-icons/bi";
+import { MdOutlinePhoto } from "react-icons/md";
+
 const Nav = () => {
-  const [activeNav,setActiveNav]= useState('#')
+  const [activeNav, setActiveNav] = useState("#");
+  const [scrollPosition, setScrollPosition] = useState(0);
+  const handleScroll = () => {
+    const position = window.pageYOffset;
+    setScrollPosition(position);
+  };
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+  console.log(scrollPosition);
+  const handleClick = () => {
+    if (activeNav !== "") {
+      setActiveNav("");
+    }
+  };
+
   return (
     <nav>
       {/* take you to the top */}
-      <a href="#" onClick={()=> setActiveNav('#')}className={activeNav ==='#'?'active':''}><HiOutlineHome/></a>
-      <a href="#about" onClick={()=> setActiveNav('#about')} className={activeNav ==='#about'?'active':''}><AiOutlineUser/></a>
-      <a href="#experience"onClick={()=> setActiveNav('#experience')} className={activeNav ==='#experience'?'active':''}><BiBook/></a>
-      <a href="#portfolio"onClick={()=> setActiveNav('#services')} className={activeNav ==='#services'?'active':''}><MdOutlinePhoto/></a>
-      <a href="#contact"onClick={()=> setActiveNav('#contact')} className={activeNav ==='#contact'?'active':''}><AiOutlineMessage/></a>
-      
+      <a
+        href="#"
+        onClick={() => handleClick()}
+        className={
+          activeNav === "#" || (scrollPosition < 155 && scrollPosition >= 0)
+            ? "active"
+            : ""
+        }
+      >
+        <HiOutlineHome />
+      </a>
+      <a
+        href="#about"
+        onClick={() => handleClick()}
+        className={
+          activeNav === "#about" ||
+          (scrollPosition > 755 && scrollPosition < 1280)
+            ? "active"
+            : ""
+        }
+      >
+        <AiOutlineUser />
+      </a>
+      <a
+        href="#experience"
+        onClick={() => handleClick()}
+        className={
+          activeNav === "#experience" ||
+          (scrollPosition > 1280 && scrollPosition < 1813)
+            ? "active"
+            : ""
+        }
+      >
+        <BiBook />
+      </a>
+      <a
+        href="#portfolio"
+        onClick={() => handleClick()}
+        className={
+          activeNav === "#services" ||
+          (scrollPosition > 1813 && scrollPosition < 3021)
+            ? "active"
+            : ""
+        }
+      >
+        <MdOutlinePhoto />
+      </a>
+      <a
+        href="#contact"
+        onClick={() => handleClick()}
+        className={
+          activeNav === "#contact" ||
+          (scrollPosition > 3077 && scrollPosition < 5000)
+            ? "active"
+            : ""
+        }
+      >
+        <AiOutlineMessage />
+      </a>
     </nav>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
